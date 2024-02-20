7fd3378be6d9a41e313ed90f44614008162b7c47
jhin93 <scene1993@gmail.com>
2024-02-20 22:13:11 +0900

    new() function

--- main.go
+++ main.go
@@ -6,5 +6,5 @@ func main() {
 	// new() function creates a variable and returns a pointer to the variable
 	ptr := new(int) // 0xc000012088
 	*ptr = 3
-	fmt.Println(*ptr) // 3
+	fmt.Println(ptr) // 3
 }
