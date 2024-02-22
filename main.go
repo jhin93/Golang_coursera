9ee81a3eac5bddae4d298b8dae28f387bd8cf9fe
jhin93 <scene1993@gmail.com>
2024-02-22 20:56:58 +0900

    정수화 완료

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "Golang_coursera/trunc"
+import (
+	"Golang_coursera/trunc"
+	"fmt"
+	"os"
+)
 
 func main() {
-	trunc.Trunc()
+	// Get Input
+	args := os.Args
+	if len(args) < 2 {
+		fmt.Println("Type floating point number")
+	}
+
+	input := args[1]
+
+	number, err := trunc.Trunc(input)
+	if err != nil {
+		return // 왜 여기에 return 0 하면 에러가 나는가
+	}
+
+	fmt.Println("Truncated number : ", number)
 }
--- trunc/trunc.go
+++ trunc/trunc.go
@@ -1,7 +1,18 @@
 package trunc
 
-import "fmt"
+import (
+	"strconv"
+)
 
-func Trunc() {
-	fmt.Println("test")
+// Trunc func take string input and return only number part.
+func Trunc(input string) (int, error) {
+	// Change string input to floating number
+	floatValue, err := strconv.ParseFloat(input, 64)
+	if err != nil {
+		return 0, err
+	}
+
+	// Truncate to integer
+	intValue := int(floatValue)
+	return intValue, nil
 }
