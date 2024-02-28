4bd57fb667fad9ab67d4a9dbdc06f108a7ffc526
jhin93 <scene1993@gmail.com>
2024-02-28 20:42:59 +0900

    empty main.go

--- slice.go
+++ slice.go
@@ -1,47 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"sort"
-	"strconv"
-	"unicode"
-)
-
 func main() {
-	// Creating Slice
-	intSlice := make([]int, 0, 3)
-	// For loop
-	for cap(intSlice) > 0 {
-		fmt.Println("Type any integer.")
-		var inputString string
-		// Error checking
-		if _, err := fmt.Scan(&inputString); err != nil {
-			fmt.Println("Error occurred:", err)
-			break
-		}
-		// If Input is "X", finish the loop
-		if inputString == "X" {
-			fmt.Println("Exiting the loop.")
-			break
-		}
-		// Checking letter between integer
-		if hasLetter(inputString) {
-			fmt.Println("Invalid input. Please enter a valid integer.")
-			continue
-		}
-		inputInt, _ := strconv.Atoi(inputString)
-		intSlice = append(intSlice, inputInt)
-		sort.Ints(intSlice)
-		fmt.Println("intSlice:", intSlice)
-	}
-}
 
-// Checking letter function
-func hasLetter(s string) bool {
-	for _, char := range s {
-		if unicode.IsLetter(char) {
-			return true
-		}
-	}
-	return false
 }
