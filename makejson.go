bc9d3a569365d695f1aa18b50d8a0bc5c9abcf7f
jhin93 <scene1993@gmail.com>
2024-03-04 21:14:30 +0900

    makejson.go complete

--- makejson.go
+++ makejson.go
@@ -1,5 +1,32 @@
 package main
 
-func main() {
+import (
+	"encoding/json"
+	"fmt"
+)
 
+func main() {
+	// Create map, name, address
+	nameAddr := make(map[string]string)
+	var inputName string
+	var inputAddr string
+
+	// Scan input from terminal
+	fmt.Println("Type your name.")
+	fmt.Scan(&inputName)
+	fmt.Println("Type your address.")
+	fmt.Scan(&inputAddr)
+
+	// Put input value into map
+	nameAddr["name"] = inputName
+	nameAddr["address"] = inputAddr
+
+	jsonData, err := json.Marshal(nameAddr)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// Use Marshal() to convert map to json
+	fmt.Println(string(jsonData))
 }
