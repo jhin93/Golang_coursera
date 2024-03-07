5c2e8724cc8634f9d2a372cf58b6ae7dd42dca58
jhin93 <scene1993@gmail.com>
2024-03-07 21:16:23 +0900

    slice contain a pointer to array

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import "fmt"
 
+func foo(sli []int) []int { // slice contain a pointer to array
+	sli[0] = sli[0] + 1
+	return sli
+}
+
+func main() {
+	a := []int{1, 2, 3}
+	fmt.Print(a) // [1 2 3]
+	foo(a)
+	fmt.Print(a) // [2 2 3]
 }
