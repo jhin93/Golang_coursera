fdf1a266a6f8510348461b921d96b4483e6ddd1b
jhin93 <scene1993@gmail.com>
2024-03-13 21:15:56 +0900

    making int slice under length 10

--- BubbleSort.go
+++ BubbleSort.go
@@ -1,27 +1,42 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"strconv"
 	"strings"
 )
 
 func main() {
 	var intSlice []int
-	var input string
-	fmt.Println("Enter no more than 10 integers. Each integers are distinguished by blank.")
-	_, err := fmt.Scanln(&input)
-	if err != nil {
-		fmt.Println("Error occur during taking integer : ", err)
+	// Making scanner object
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Println("Type no more than 10 integers. Each integers are distinguished by blank.")
+	// Scan the input that the user typed
+	scanner.Scan()
+	// Read the contents of input
+	inputLine := scanner.Text()
+	// Checking Err
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Error occur during taking input: ", err)
 		return
 	}
-	stringInputs := strings.Fields(input)
-	for _, i := range stringInputs {
-		num, err := strconv.Atoi(i)
+	// Making Slice of integers
+	stringSlice := strings.Fields(inputLine)
+
+	for _, eachInt := range stringSlice {
+		num, err := strconv.Atoi(eachInt)
 		if err != nil {
-			fmt.Println("Error occur during convert Ascii to int : ", err)
+			fmt.Println("Error occur during converting string to int: ", err)
+			continue
 		}
 		intSlice = append(intSlice, num)
 	}
-	fmt.Println(intSlice)
+	// Checking whether the length of the slice over 10
+	if len(intSlice) > 10 {
+		fmt.Println("Do not type more than 10 integers. Try again")
+		return
+	}
+
 }
