a4d61283ca0d708cd9121b6c0d6df4538f737563
jhin93 <scene1993@gmail.com>
2024-04-01 21:56:03 +0900

    complete

--- main.go
+++ main.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
 type Animal interface {
 	Eat()
@@ -53,6 +58,69 @@ func (b Bird) Speak() {
 	fmt.Println(b.Sound)
 }
 
-func main() {
+func (s Snake) Eat() {
+	fmt.Println(s.Food)
+}
 
+func (s Snake) Move() {
+	fmt.Println(s.Locomotion)
+}
+
+func (s Snake) Speak() {
+	fmt.Println(s.Sound)
+}
+
+func main() {
+	newAnimal := make(map[string]Animal)
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Println(">")
+		scanner.Scan()
+		inputLine := scanner.Text()
+		if err := scanner.Err(); err != nil {
+			fmt.Println("Error occured while scanning typed input", err)
+		}
+		stringSlice := strings.Fields(inputLine)
+
+		if len(stringSlice) != 3 {
+			fmt.Println("Invalid Input. Please enter at least three words. First word must be 'newanimal' or 'query'.")
+			return
+		}
+
+		switch stringSlice[0] {
+		case "newanimal":
+			switch stringSlice[2] {
+			case "cow":
+				newAnimal[stringSlice[1]] = Cow{Name: stringSlice[1], Food: "grass", Locomotion: "walk", Sound: "moo"}
+				fmt.Println("Created it!")
+			case "bird":
+				newAnimal[stringSlice[1]] = Bird{Name: stringSlice[1], Food: "worms", Locomotion: "fly", Sound: "peep"}
+				fmt.Println("Created it!")
+			case "snake":
+				newAnimal[stringSlice[1]] = Bird{Name: stringSlice[1], Food: "mice", Locomotion: "slither", Sound: "hsss"}
+				fmt.Println("Created it!")
+			default:
+				fmt.Println("Invalid animal type. Please enter 'cow', 'bird', or 'snake'.")
+			}
+
+		case "query":
+			animal, ok := newAnimal[stringSlice[1]]
+			if !ok {
+				fmt.Println("Animal not found. Try again.")
+				continue
+			}
+			switch stringSlice[2] {
+			case "eat":
+				animal.Eat()
+			case "move":
+				animal.Move()
+			case "speak":
+				animal.Speak()
+			default:
+				fmt.Println("Invalid action. Please enter 'eat', 'move', or 'speak'.")
+			}
+		default:
+			fmt.Println("First word must be 'newanimal' or 'query'.")
+		}
+	}
 }
