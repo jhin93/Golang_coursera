587f1b4a96aaadb93bc85964ae65269f5288f8af
jhin93 <scene1993@gmail.com>
2024-04-01 21:17:00 +0900

    Declaring all types

--- main.go
+++ main.go
@@ -1,5 +1,58 @@
 package main
 
+import "fmt"
+
+type Animal interface {
+	Eat()
+	Move()
+	Speak()
+}
+
+type Cow struct {
+	Name       string
+	Food       string
+	Locomotion string
+	Sound      string
+}
+
+type Bird struct {
+	Name       string
+	Food       string
+	Locomotion string
+	Sound      string
+}
+
+type Snake struct {
+	Name       string
+	Food       string
+	Locomotion string
+	Sound      string
+}
+
+func (c Cow) Eat() {
+	fmt.Println(c.Food)
+}
+
+func (c Cow) Move() {
+	fmt.Println(c.Locomotion)
+}
+
+func (c Cow) Speak() {
+	fmt.Println(c.Sound)
+}
+
+func (b Bird) Eat() {
+	fmt.Println(b.Food)
+}
+
+func (b Bird) Move() {
+	fmt.Println(b.Locomotion)
+}
+
+func (b Bird) Speak() {
+	fmt.Println(b.Sound)
+}
+
 func main() {
 
 }
