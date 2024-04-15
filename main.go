31fd66c5aefb334011f6b41a1a51791c383511fe
jhin93 <scene1993@gmail.com>
2024-04-15 12:51:38 +0900

    Mutex 적용

--- main.go
+++ main.go
@@ -7,9 +7,12 @@ import (
 
 var i int = 0
 var wg sync.WaitGroup
+var mut sync.Mutex // 뮤텍스 변수 선언
 
 func inc() {
+	mut.Lock() // 뮤텍스로 변수 i의 접근을 잠금
 	i = i + 1
+	mut.Unlock() // 잠금 해제
 	wg.Done()
 }
 
@@ -18,5 +21,5 @@ func main() {
 	go inc()
 	go inc()
 	wg.Wait()
-	fmt.Println(i)
+	fmt.Println(i) // 예상 결과는 항상 2가 출력
 }
