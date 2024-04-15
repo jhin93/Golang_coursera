99c607902c1bc4ac994e265df3dfc862e6ab9716
jhin93 <scene1993@gmail.com>
2024-04-15 12:51:07 +0900

    sync.WaitGroup() 시ㄹ행

--- main.go
+++ main.go
@@ -1,73 +1,22 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
-	"sort"
-	"strconv"
-	"strings"
 	"sync"
 )
 
-func splitSliceEqually(s []int, parts int) [][]int {
-	var result [][]int
-	totalLen := len(s)
-	baseSize := totalLen / parts
-	remainder := totalLen % parts
+var i int = 0
+var wg sync.WaitGroup
 
-	start := 0
-	for i := 0; i < parts; i++ {
-		end := start + baseSize
-		if i < remainder {
-			end++
-		}
-		result = append(result, s[start:end])
-		start = end
-	}
-	return result
+func inc() {
+	i = i + 1
+	wg.Done()
 }
 
 func main() {
-	var intSlice []int
-	scanner := bufio.NewScanner(os.Stdin)
-	fmt.Println("Type at least 4 integers. Each integers are distinguished by blank.")
-	scanner.Scan()
-	inputLine := scanner.Text()
-	// Checking Err
-	if err := scanner.Err(); err != nil {
-		fmt.Println("Error occur during taking input: ", err)
-		return
-	}
-	stringSlice := strings.Fields(inputLine)
-
-	// Making Slice of integers
-	for _, i := range stringSlice {
-		num, err := strconv.Atoi(i)
-		if err != nil {
-			fmt.Println("wrong int during strconv:", i)
-			continue
-		}
-		intSlice = append(intSlice, num)
-	}
-
-	splitSlices := splitSliceEqually(intSlice, 4)
-
-	var wg sync.WaitGroup
-	for _, split := range splitSlices {
-		wg.Add(1)
-		go func(s []int) {
-			defer wg.Done()
-			sort.Ints(s)
-		}(split) // go 익명 함수를 split 슬라이스를 인자로 하여 즉시 호출
-		fmt.Println("Splited Slice : ", split)
-	}
+	wg.Add(2)
+	go inc()
+	go inc()
 	wg.Wait()
-
-	var mergedSlice []int
-	for _, split := range splitSlices {
-		mergedSlice = append(mergedSlice, split...)
-		sort.Ints(mergedSlice)
-	}
-	fmt.Println("Result : ", mergedSlice)
+	fmt.Println(i)
 }
