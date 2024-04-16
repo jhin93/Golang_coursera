b7b8bde692fae681da906f0f6157da3b416fdc64
jhin93 <scene1993@gmail.com>
2024-04-16 21:56:06 +0900

    중간 커밋

--- main.go
+++ main.go
@@ -1,31 +1,56 @@
 package main
 
 import (
-    "fmt"
-    "time"
+	"fmt"
+	"sync"
 )
 
+var wg sync.WaitGroup
+
+type ChopS struct {
+	sync.Mutex
+}
+
+type Philo struct {
+	leftCS, rightCS *ChopS
+}
+
+func (p Philo) eat(wg *sync.WaitGroup) {
+	// 3번만 먹는다는 조건 필요
+	// 3번 다 먹으면 defer wg.Done() 실행으로 waitGroup 감소
+	defer wg.Done()
+	for i := 0; i < 3; i++ {
+		p.leftCS.Lock()
+		p.rightCS.Lock()
+
+		fmt.Println("eating")
+
+		p.rightCS.Unlock()
+		p.leftCS.Unlock()
+
+		fmt.Println("finish")
+	}
+}
+
 func main() {
-    c1 := make(chan string)
-    c2 := make(chan string)
+	wg.Add(5)
 
-    go func() {
-        time.Sleep(1 * time.Second)
-        c1 <- "one"
-    }()
-    go func() {
-        time.Sleep(2 * time.Second)
-        c2 <- "two"
-    }()
+	// Making 5 Chopsticks
+	CSticks := make([]*ChopS, 5)
+	for i := 0; i < 5; i++ {
+		CSticks[i] = new(ChopS)
+	}
 
-    for i := 0; i < 2; i++ {
-        select {
-        case msg1 := <-c1:
-            fmt.Println("Received", msg1)
-        case msg2 := <-c2:
-            fmt.Println("Received", msg2)
-        case <-time.After(3 * time.Second):
-            fmt.Println("timeout")
-        }
-    }
+	// Making 5 Philosophers
+	philos := make([]*Philo, 5)
+	for i := 0; i < 5; i++ {
+		philos[i] = &Philo{leftCS: CSticks[i], rightCS: CSticks[(i+1)%5]}
+	}
+
+	for _, philo := range philos {
+		go philo.eat(&wg)
+	}
+
+	wg.Wait()
+
 }
